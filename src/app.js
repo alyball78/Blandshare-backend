6dec951375590bac5423f8fc710e5498afee8172
alyball78 <aly.ball@laplateforme.io>
2026-07-15 10:02:59 +0200

    feat: gestion d'erreurs et validation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import express from 'express';
-                                
+import cors from 'cors';
+
+import errorHandler from './middlewares/errorHandler.js';
+
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json());
+
+// TODO : brancher les routes ici au fil des étapes
+
+app.use(errorHandler);
 
 export default app;
